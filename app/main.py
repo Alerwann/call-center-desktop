3b3bf91ebd9b3f3530878c82ba4573d0562035db
Alerwann <alerwann411@gmail.com>
2025-08-25 18:57:20 +0200

    feature: creation du fichier main

--- app/main.py
+++ app/main.py
@@ -9,15 +9,21 @@ import uvicorn
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from app.config.settings import settings
 from app.core.database import connect_database
+from app.api.v1.router import api_router
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
+    """
+        function faisant la connection à la base de donnée
+        note: fonction async
+    """
     await connect_database()
     print(f"🚀 Server running on port {settings.port}")
     yield
     print("🛑 Shutting down...")
 
 def create_app() -> FastAPI:
+    """Creation  du cors pour connexion server"""
     app = FastAPI(title="HR System API", version="1.0.0", lifespan=lifespan)
 
     # CORS (équivalent de ton app.use(cors))
@@ -28,7 +34,7 @@ def create_app() -> FastAPI:
         allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
         allow_headers=["*"],
     )
-
+    app.include_router(api_router)
     return app
 
 
